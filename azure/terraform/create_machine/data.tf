7fe744f1a9ca428a4b910c3d33213c7cb365c21c
XandeCoding <af.santos1998@gmail.com>
2021-10-19 23:02:25 -0300

    adicionado alguns recursos básicos de rede

--- azure/terraform/create_machine/data.tf
+++ azure/terraform/create_machine/data.tf
@@ -1,16 +1,37 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "resource_group" {
   name     = "var.resource_group_name"
-  location = "westus2"
+  location = "var.location"
 
   tags = {
-      Environment = "Terraform Getting Started"
-      Team = "DevOps"
+      Environment = "Terraform game server"
   }
 }
 
-resource "azurerm_virtual_network" "vnet" {
-    name = "myTFVnet"
-    address_space = ["10.0.0.0/16"]
-    location = "westus2"
-    resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_virtual_network" "virtual_network" {
+  name = "var.virtual_network_name"
+  address_space = ["10.0.0.0/16"]
+  location = "var.location"
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  tags = {
+    Environment = "Terraform game server"
+  }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name = "var.subnet_name"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  address_prefixes = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name = "var.public_ip_name"
+  location = "var.location"
+  resource_group_name = "azurerm_resource_group.resource_group.name"
+  allocation_method = "Dynamic"
+
+  tags = {
+    Environment = "Terraform game server"
+  }
 }
--- azure/terraform/create_machine/variables.tf
+++ azure/terraform/create_machine/variables.tf
@@ -1,3 +1,21 @@
+# Resource names
 variable "resource_group_name" {
-  default = "myTFResourceGroup"
+  default = "game_resource_group"
+}
+
+variable "virtual_network_name" {
+  default = "game_virtual_network"
+}
+
+variable "subnet_name" {
+  default = "game_subnet"
+}
+
+variable "public_ip_name" {
+  default = "game_public_ip"
+}
+
+# Others
+variable "location" {
+  default = "brazilsouth"
 }
