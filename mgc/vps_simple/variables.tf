47fc6adb50183041408b1f20ae6cfa9026f9cab6
XandeCoding <af.santos1998@gmail.com>
2025-12-27 18:44:02 -0300

    feat: new vps via terraform

---
+++ mgc/vps_simple/variables.tf
@@ -0,0 +1,9 @@
+variable "mgc_api_key" {
+  description = "The key from mgc api"
+  type        = string
+}
+
+variable "mgc_ssh_key" {
+  description = "SSH key from mgc"
+  type        = string
+}
